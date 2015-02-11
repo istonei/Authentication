d0dd0e98d8fcb00199ed6770e3ffd557ccf23545
istonei <jeewfayce@yahoo.com>
2015-02-11 18:02:12 -0500

    Update AuthenticationTest.java

--- Authentication/src/AuthenticationTest.java
+++ Authentication/src/AuthenticationTest.java
@@ -2,7 +2,9 @@
 public class AuthenticationTest {
 
 	public static void main(String[] args) {
+		//Call userAuthentication method
 		Authentication.userAuthentication();
+		//Call userAuthentication method
 		Authentication.prompt();
 
 	}
